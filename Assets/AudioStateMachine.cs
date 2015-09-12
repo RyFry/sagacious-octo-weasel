05bf0c436f2624daef0f97ea8451f5a43bfee039
RyFry <ryanlorey@outlook.com>
2015-09-11 21:58:26 -0500

    Adding audio files and AudioState scripts to Assets folder

---
+++ Assets/AudioStateMachine.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioStateMachine : MonoBehaviour {
+
+	public enum AudioResponse {
+		Yes,
+		No,
+		Spit
+	};
+
+	private AudioState currentAudioState;
+
+	// Use this for initialization
+	void Start () {
+		this.currentAudioState = GameObject.Find ("Main Camera").GetComponent<AudioState>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ChangeAudioState (AudioResponse response) {
+		// Don't accept input if audio is already playing
+		if (this.currentAudioState.audio.isPlaying)
+			return;
+
+		switch (response) {
+		case AudioResponse.Yes:
+			this.currentAudioState.audio.clip = this.currentAudioState.yes;
+			break;
+		case AudioResponse.No:
+			this.currentAudioState.audio.clip = this.currentAudioState.no;
+			break;
+		case AudioResponse.Spit:
+			this.currentAudioState.audio.clip = this.currentAudioState.spit;
+			break;
+		}
+	}
+}
