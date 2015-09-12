fcbb0f31c1825d7e832ca317b9002986b565c6f5
RyFry <ryanlorey@outlook.com>
2015-09-12 13:41:55 -0500

    Moving scripts to scripts folder

--- Assets/scripts/AudioState.cs
+++ Assets/scripts/AudioState.cs
@@ -2,21 +2,40 @@
 using System.Collections;
 
 public class AudioState : MonoBehaviour {
+
+	public AudioState nextAudioState;
+
 	public AudioSource audio;
 	public AudioClip yes;
 	public AudioClip no;
 	public AudioClip spit;
 
 	// Use this for initialization
-	void Start () {
+	public void StartIntro () {
 		this.audio = GetComponent<AudioSource> ();
 		this.audio.Play();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public void StartYes () {
 		if (! this.audio.isPlaying) {
 			this.audio.clip = yes;
+			this.audio.Play();
+		}
+	}
+
+	// Update is called once per frame
+	public void StartNo () {
+		if (! this.audio.isPlaying) {
+			this.audio.clip = no;
+			this.audio.Play();
+		}
+	}
+
+	// Update is called once per frame
+	public void StartSpit () {
+		if (! this.audio.isPlaying) {
+			this.audio.clip = spit;
 			this.audio.Play();
 		}
 	}
